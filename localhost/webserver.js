367f219a91e8f0f8658635b9e53e0cdbca2afd18
ruxyolo <johabaltha@gmail.com>
2022-02-06 21:09:21 +0100

    4.2.1

--- localhost/webserver.js
+++ localhost/webserver.js
@@ -3,12 +3,12 @@ const express = require('express')
 const app = express()
 let port = 3000
 
-app.get('/', function(req, res) {
-    res.sendStatus(200)
-    res.sendFile('index.html')
-});
-
 function startHost() {
+    app.get('/', function(req, res) {
+        res.sendStatus(200)
+        res.sendFile('index.html')
+    });
+
     app.listen(port, function() {
         console.log('Server is running on localhost' + port)
     });
